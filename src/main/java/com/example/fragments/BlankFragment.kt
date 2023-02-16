ea2ea3fbeef4a75769f4defd72d7e633ce745663
TeaOk210 <danilplay21006@gmail.com>
2023-02-16 19:24:22 +0200

    Frags

--- src/main/java/com/example/fragments/BlankFragment.kt
+++ src/main/java/com/example/fragments/BlankFragment.kt
@@ -14,7 +14,7 @@ class BlankFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+        return inflater.inflate(R.layout.fragment_1, container, false)
     }
 
     companion object {
